aba71e196f5e72ff1e7014b66979e160f1c1c6e1
S4nsx1 <benjamincancino90@gmail.com>
2024-11-05 17:05:47 -0300

    Update usuarios_gimnasio.py

--- usuarios_gimnasio.py
+++ usuarios_gimnasio.py
@@ -16,7 +16,7 @@ df_nobinarios = df[df["gender"] == "Non-binary"]
 cantidad_nobinarios = len(df_nobinarios)
 
 fig,ax = plt.subplots()
-ax.bar(["Hombre","Mujer","No binario"], [cantidad_hombres,cantidad_mujeres,cantidad_nobinarios], color=blue)
+ax.bar(["Hombre","Mujer","No binario"], [cantidad_hombres,cantidad_mujeres,cantidad_nobinarios], color="blue")
 ax.set_xlabel("genero")
 ax.set_ylabel("Cantidad")
 ax.set_tittle("Generos usuarios registrados")
