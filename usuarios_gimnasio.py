2b6c7a492cb50ecc8a27d8966b02c4fb1f9f8e90
S4nsx1 <benjamincancino90@gmail.com>
2024-11-05 17:14:21 -0300

    Update usuarios_gimnasio.py

--- usuarios_gimnasio.py
+++ usuarios_gimnasio.py
@@ -6,19 +6,18 @@ df = pd.read_csv("users_data.csv")
 
 st.write("""## Graficas de los usuarios que asisten al gimnasio""")
 if st.button("Generos"("grafico")):
+  df_hombres = df[df["gender"] == "Male"]
+  cantidad_hombres = len(df_hombres)
+
+  df_mujeres = df[df["gender"] == "Female"]
+  cantidad_mujeres = len(df_mujeres)
+
+  df_nobinarios = df[df["gender"] == "Non-binary"]
+  cantidad_nobinarios = len(df_nobinarios)
+
+  fig,ax = plt.subplots()
+  ax.bar(["Hombre","Mujer","No binario"], [cantidad_hombres,cantidad_mujeres,cantidad_nobinarios], color="blue")
+  ax.set_xlabel("genero")
+  ax.set_ylabel("Cantidad")
+  ax.set_title("Generos usuarios registrados")
   st.pyplot(fig)
-df_hombres = df[df["gender"] == "Male"]
-cantidad_hombres = len(df_hombres)
-
-df_mujeres = df[df["gender"] == "Female"]
-cantidad_mujeres = len(df_mujeres)
-
-df_nobinarios = df[df["gender"] == "Non-binary"]
-cantidad_nobinarios = len(df_nobinarios)
-
-fig,ax = plt.subplots()
-ax.bar(["Hombre","Mujer","No binario"], [cantidad_hombres,cantidad_mujeres,cantidad_nobinarios], color="blue")
-ax.set_xlabel("genero")
-ax.set_ylabel("Cantidad")
-ax.set_title("Generos usuarios registrados")
-
