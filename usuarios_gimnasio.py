f2e4ddaa590a9450cdbfe4b6370bc7863b16bef4
S4nsx1 <benjamincancino90@gmail.com>
2024-11-05 16:35:53 -0300

    Update usuarios_gimnasio.py

--- usuarios_gimnasio.py
+++ usuarios_gimnasio.py
@@ -1,1 +1,7 @@
+import streamlit as st
+import pandas as pd 
+import matplotlib.pyplot as plt
 
+df = pd.read_csv("users_data.csv")
+
+st.write("""Graficas de los usuarios que asisten al gimnasio""")
