5dcb8ff5805e2193584668d80fec54c582671007
S4nsx1 <benjamincancino90@gmail.com>
2024-11-05 16:57:32 -0300

    Update usuarios_gimnasio.py

--- usuarios_gimnasio.py
+++ usuarios_gimnasio.py
@@ -1,7 +1,24 @@
 import streamlit as st
 import pandas as pd 
+import matplotlib.pyplot as plt
 
 
 df = pd.read_csv("users_data.csv")
 
 st.write("""## Graficas de los usuarios que asisten al gimnasio""")
+
+df_hombres = df[df["gender"] == "male"]
+cantidad_hombres = len(df_hombres)
+
+df_mujeres = df[df["gender"] == "female"]
+cantidad_mujeres = len(df_mujeres)
+
+df_nobinarios = df[df["gender"] == "Non-binary"]
+cantidad_nobinarios = len(df_nobinarios)
+
+ax[1].bar(["Hombre","Mujer","No binario"], [cantidad_hombres,cantidad_mujeres,cantidad_nobinarios], color=blue)
+ax[1].set_xlabel("genero")
+ax[1].set_ylabel("Cantidad")
+ax[1].set_tittle("Generos usuarios registrados")
+
+st.pyplot(fig)
