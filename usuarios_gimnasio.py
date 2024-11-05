46031b25dbfbd046bc5b9429b7d53e7ad8d76442
S4nsx1 <benjamincancino90@gmail.com>
2024-11-05 16:43:09 -0300

    Update usuarios_gimnasio.py

--- usuarios_gimnasio.py
+++ usuarios_gimnasio.py
@@ -4,4 +4,4 @@ import pandas as pd
 
 df = pd.read_csv("users_data.csv")
 
-st.write("""Graficas de los usuarios que asisten al gimnasio""")
+st.write("""##Graficas de los usuarios que asisten al gimnasio""")
